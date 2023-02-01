44b6492f417cb75c8cb122b5ec5dee3d3aeee482
100dongwoo <qorehddn123@naver.com>
2023-02-01 19:32:56 +0900

    add migrating from restAPI to graphql

--- tweetql/server.js
+++ tweetql/server.js
@@ -1,48 +1,102 @@
 import { ApolloServer, gql } from "apollo-server";
+import fetch from "node-fetch";
 
 const tweets = [
   {
     id: "1",
     text: "hello first",
+    userId: "2",
   },
   {
     id: "2",
     text: "hello second",
+    userId: "2",
+  },
+];
+let users = [
+  {
+    id: "1",
+    firstName: "100",
+    lastName: "dong",
+  },
+  {
+    id: "2",
+    firstName: "1001",
+    lastName: "dong3",
   },
 ];
 
 //SDL
 const typeDefs = gql`
   type User {
-    id: ID
-    username: String
+    id: ID!
+    firstName: String!
+    lastName: String!
+    fullName: String!
   }
+  """
+  Tweet object represents a resource for a Tweet 설명이 가능하다
+  """
   type Tweet {
     id: ID!
     text: String!
     author: User
   }
   type Query {
+    """
+    all 유저~~
+    """
+    allUsers: [User!]!
     allTweets: [Tweet!]!
     tweet(id: ID!): Tweet
+    allMovies: [Movie!]!
+    movie(id: String!): Movie
   }
 
   type Mutation {
     postTweet(text: String!, userId: ID!): Tweet!
     deleteTweet(id: ID!): Boolean!
   }
+
+  type Movie {
+    id: Int!
+    url: String!
+    imdb_code: String!
+    title: String!
+  }
 `;
 
 const resolvers = {
   Query: {
+    allMovies() {
+      return fetch("https://yts.mx/api/v2/list_movies.json")
+        .then((r) => r.json())
+        .then((json) => json.data.movies);
+    },
+    movie(_, { id }) {
+      return fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+        .then((r) => r.json())
+        .then((json) => json.data.movie);
+    },
+    allUsers() {
+      return users;
+    },
+
     allTweets() {
       return tweets;
     },
-    tweet(
-      root,
-      { id } // args
-    ) {
+    tweet(root, { id }) {
       return tweets.find((data) => data.id === id);
+    },
+  },
+  User: {
+    fullName({ firstName, lastName }) {
+      return `${firstName} ${lastName}`;
+    },
+  },
+  Tweet: {
+    author({ userId }) {
+      return users.find((user) => user.id === userId);
     },
   },
 
